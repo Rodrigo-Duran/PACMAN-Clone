f5dd023cee413ae2b1e7f6ff444d31e48cbf7d6b
Rodrigo Duran <rodrigoddaniel00@gmail.com>
2024-04-03 15:46:26 -0300

    Final-Version
    Final Version of the project

--- Pixel PACMAN/Assets/Scripts/Ghost.cs
+++ Pixel PACMAN/Assets/Scripts/Ghost.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
